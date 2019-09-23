9fe2de527476088cb7d443f4da94034b760f5d37
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-23 17:06:30 +0200

    bug in data dir

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,4 +3,4 @@ BASE_DIR=${SCRIPTS_DIR}/..
 
 source ${SCRIPTS_DIR}/config.sh
 
-sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${DATA_DIR}
+sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${SOCKEYE_DIR}
