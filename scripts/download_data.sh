8e0e639e26ce09d71f350c74035a19632c1c7938
Markus Göckeritz <markus@goeckeritz.ch>
2019-09-23 18:20:39 +0200

    minor changes

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -4,3 +4,6 @@ BASE_DIR=${SCRIPTS_DIR}/..
 source ${SCRIPTS_DIR}/config.sh
 
 sockeye-autopilot --task ${LANGUAGE_DATA} --model none --workspace ${SOCKEYE_DIR}
+
+cd ${SOCKEYE_DIR}/systems/${LANGUAGE_DATA}/data/bpe
+gunzip *
