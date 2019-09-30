1bd9ae1ca4a9796c1fac3f0e4a8d3cfd7e11baa3
Markus Göckeritz <markus@dhcp-wlan-uzh-10-12-222-019.uzh.ch>
2019-09-30 10:13:11 +0200

    added bleu back

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -9,4 +9,6 @@ python OpenNMT-py/translate.py \
   --model models/bahdanau-14_step_100000.pt \
   --src sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src \
   --tgt sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.trg \
-  --output output/pred.txt
+  --output output/pred.txt \
+  --report_bleu \
+  --report_time
