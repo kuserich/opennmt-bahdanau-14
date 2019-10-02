1abf740ad55e87bf7f336e835df4d97f6e1cbf91
Markus Göckeritz <markus@markuss-mbp.home>
2019-10-02 17:39:32 +0200

    added gpu to eval

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,12 +3,13 @@ BASE_DIR=${SCRIPTS_DIR}/..
 
 source ${SCRIPTS_DIR}/config.sh
 
+export CUDA_VISIBLE_DEVICES=0,1
+
 mkdir output
 
 python OpenNMT-py/translate.py \
   --model models/bahdanau-14_step_100000.pt \
+  --gpu 0 \
   --src sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.src \
   --tgt sockeye_autopilot/systems/wmt14_en_de/data/bpe/dev.trg \
-  --output output/pred.txt \
-  --report_bleu \
-  --report_time
+  --output output/pred.txt
